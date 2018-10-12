f1a270542ae554c6dda687bfaae9700dbb44a297
zhijun.zhou <zhijun.zhou@ubtrobot.com>
2018-10-12 15:51:51 +0800

    [other]fix bug

--- app/src/main/java/com/ubtechinc/goldenpig/pigmanager/register/GetPairPigQRHttpProxy.java
+++ app/src/main/java/com/ubtechinc/goldenpig/pigmanager/register/GetPairPigQRHttpProxy.java
@@ -1,5 +1,8 @@
 package com.ubtechinc.goldenpig.pigmanager.register;
 
+import android.content.Context;
+import android.os.Handler;
+
 import com.ubtechinc.goldenpig.net.BaseHttpProxy;
 import com.ubtechinc.nets.BuildConfig;
 
@@ -12,55 +15,59 @@ import okhttp3.Request;
 import okhttp3.Response;
 
 /**
- *@auther        :hqt
- *@email         :qiangta.huang@ubtrobot.com
- *@description   :获取生成添加成员的二维码
- *@time          :2018/9/22 14:17
- *@change        :
- *@changetime    :2018/9/22 14:17
-*/
+ * @auther :hqt
+ * @email :qiangta.huang@ubtrobot.com
+ * @description :获取生成添加成员的二维码
+ * @time :2018/9/22 14:17
+ * @change :
+ * @changetime :2018/9/22 14:17
+ */
 public class GetPairPigQRHttpProxy extends BaseHttpProxy {
-    public void getPairPigQR(String token,String appId,final GetPairPigQRCallBack callBack){
-        OkHttpClient okHttpClient =getHttpClient();
+    public void getPairPigQR(Context context, String token, String appId, final GetPairPigQRCallBack callBack) {
+        OkHttpClient okHttpClient = getHttpClient();
 
         final Request okrequest = new Request.Builder()
-                .url(BuildConfig.HOST+"/user-service-rest/v2/goldenPig/getPairInfo")
+                .url(BuildConfig.HOST + "/user-service-rest/v2/goldenPig/getPairInfo")
                 .get()
-                .addHeader("authorization",token)
-                .addHeader("X-UBT-AppId",appId)
-                .addHeader("product",BuildConfig.product)
+                .addHeader("authorization", token)
+                .addHeader("X-UBT-AppId", appId)
+                .addHeader("product", BuildConfig.product)
                 .build();
         Call call = okHttpClient.newCall(okrequest);
         call.enqueue(new Callback() {
+
+            Handler mHandler = new Handler(context.getMainLooper());
+
             @Override
             public void onFailure(Call call, IOException e) {
-                if (callBack!=null){
-                    callBack.onError(e.getMessage());
+                if (callBack != null) {
+                    mHandler.post(() -> callBack.onError(e.getMessage()));
+
                 }
             }
 
             @Override
             public void onResponse(Call call, Response response) throws IOException {
-                if (callBack!=null) {
-                    if (response.isSuccessful()){
+                if (callBack != null) {
+                    if (response.isSuccessful()) {
                         try {
                             String result = response.body().source().readUtf8();
-
+                            mHandler.post(() -> callBack.onSuccess(result));
                         } catch (RuntimeException e) {
-                            callBack.onError(e.getMessage());
+                            mHandler.post(() -> callBack.onError(e.getMessage()));
                         }
-                    }else {
-                        callBack.onError(response.message());
+                    } else {
+                        mHandler.post(() -> callBack.onError(response.message()));
                     }
-
                 }
 
             }
         });
     }
 
-    public interface GetPairPigQRCallBack{
+    public interface GetPairPigQRCallBack {
         void onError(String error);
+
         void onSuccess(String response);
     }
 }
