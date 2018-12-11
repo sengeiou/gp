1cd5a22e301e1238dd303e2bb3d6305ce63f155c
zzj <zhijun.zhou@ubtrobot.com>
2018-12-11 21:06:42 +0800

    [other] fix bug

--- app/src/main/java/com/ubtechinc/goldenpig/personal/remind/RemindActivity.java
+++ app/src/main/java/com/ubtechinc/goldenpig/personal/remind/RemindActivity.java
@@ -1,7 +1,6 @@
 package com.ubtechinc.goldenpig.personal.remind;
 
 import android.app.Activity;
-import android.content.Intent;
 import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
