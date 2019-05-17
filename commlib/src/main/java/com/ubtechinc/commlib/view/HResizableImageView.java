955e42d65227b75828961519db22067b69ad68e2
e <weimin.ma@ubtrobot.com>
2019-05-17 12:02:50 +0800

    [fix]八戒攻略增加点击效果和众创空间处理图片变形

---
+++ commlib/src/main/java/com/ubtechinc/commlib/view/HResizableImageView.java
@@ -0,0 +1,37 @@
+package com.ubtechinc.commlib.view;
+
+import android.annotation.SuppressLint;
+import android.content.Context;
+import android.graphics.drawable.Drawable;
+import android.util.AttributeSet;
+import android.widget.ImageView;
+
+@SuppressLint("AppCompatCustomView")
+public class HResizableImageView extends ImageView {
+	public HResizableImageView(Context context, AttributeSet attrs) {
+		super(context, attrs);
+	}
+
+	public HResizableImageView(Context context) {
+		super(context);
+	}
+
+	public HResizableImageView(Context context, AttributeSet attrs, int defStyle) {
+		super(context, attrs, defStyle);
+	}
+	@Override
+	protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+		Drawable d = getDrawable();
+
+		if (d != null) {
+			// ceil not round - avoid thin vertical gaps along the left/right
+			// edges
+			int width = MeasureSpec.getSize(widthMeasureSpec);
+			int height = (int) Math.ceil((float) width * (float) d.getIntrinsicHeight()
+					/ (float) d.getIntrinsicWidth());
+			setMeasuredDimension(width, height);
+		} else {
+			super.onMeasure(widthMeasureSpec, heightMeasureSpec);
+		}
+	}
+}
